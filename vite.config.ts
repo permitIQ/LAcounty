19908f1237be58f99c3f9348ab4f4565a7a64fd4
ajit <ajit@urbanlogiq.com>
2025-06-05 06:30:13 -0700

    Latest updates before deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
  
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/LAcounty/',
   plugins: [react()],
-  base: '/reactpermitiq-app/' // Adjust this to your repository name for GitHub Pages
 }) 
